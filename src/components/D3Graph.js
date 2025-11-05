f53d715938d4328671ca9576c6bc2c0b691cfbe5
bassy030 <bassy030@mymail.unisa.edu.au>
2025-11-05 19:00:52 +1030

    Added D3 data fetching.
    Added state for rngNumber and engArray with 50 limits and implmeneted array shift logic to maintain maxItem limit.

--- src/components/D3Graph.js
+++ src/components/D3Graph.js
@@ -1,13 +1,36 @@
-import React, { useState, useEffect } from "react";
+﻿import React, { useState, useEffect } from "react";
 import console_monkey_patch, { getD3Data } from "../console-monkey-patch";
 import * as d3 from "d3";
 
 export default function D3Graph() {
-    const[rngNumber, setRngNumber] = useState(0);
+    const[rngNumber, setRngNumber] = useState("");
     const [rngArray, setRngArray] = useState([]);
     const maxItems = 50;
     const timeOut = 100;
     const maxValue = 1;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const getData = getD3Data();  
+            if (getData.length > 0) {
+                setRngNumber(getData[getData.length - 1]);
+            }
+        }, timeOut);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (rngNumber) {
+            setRngArray((previousArray) => {
+                let temporrayArray = [...previousArray, rngNumber];
+                if (temporrayArray.length > maxItems) {
+                    temporrayArray.shift()
+                }
+                return temporrayArray;
+            });
+        }
+    }, [rngNumber]);
 
     return (
         <div className="card shadow mt-3">
@@ -15,6 +38,9 @@ export default function D3Graph() {
                 <h5>Live Music Visualization</h5>
             </div>
             <div className="card-body">
+                <h1>
+                    Output : {rngNumber}
+                </h1>
                 <svg width="100%" height="400px" className="border border-primary"></svg>
             </div>
         </div>
