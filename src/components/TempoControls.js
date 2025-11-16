98a9b0ed0d61bbe1d3061fba310e6b4a68554430
bassy030 <bassy030@mymail.unisa.edu.au>
2025-11-16 15:36:36 +1030

    Enhance TempoControls UI styling.
    Updated TempoControls component to improve visual appearance.

--- src/components/TempoControls.js
+++ src/components/TempoControls.js
@@ -1,27 +1,27 @@
 function TempControls({ bpm, setBpm, conversion, setConversion, beatCycle, setBeatCycle }) {
     return (
-        <div className="card shadow mt-3">
-            <div className="card-header bg-info">
+        <div className="card shadow mt-3" style={{ backgroundColor: '#87CEEB' }} >
+            <div className="card-header bg-dark text-white">
                 <h3><i className="bi bi-speedometer2 me-2"></i> Tempo Controls</h3>
             </div>
             <div className="card-body">
                 <div className="mb-3">
-                    <label htmlFor="inputBpm" className="form-label">
+                    <label htmlFor="inputBpm" className="form-label" style={{ fontSize: '1.4rem', fontWeight: 'bold' }}>
                         BPM (Tempo) 
                     </label>
-                    <input type="number" className="form-control" id="inputBpm" max="200" value={bpm} onChange={(e) => setBpm(parseInt(e.target.value))}/>
+                    <input type="number" className="form-control" id="inputBpm" max="200" value={bpm} onChange={(e) => setBpm(parseInt(e.target.value))} style={{ fontSize: '1.4rem', fontWeight: '700' }} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="inputConversion" className="form-label">
+                    <label htmlFor="inputConversion" className="form-label" style={{ fontSize: '1.4rem', fontWeight: 'bold' }}>
                         Conversion Factor
                     </label>
-                    <input type="number" className="form-control" id="inputConversion" value={conversion} onChange={(e) => setConversion(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" id="inputConversion" value={conversion} onChange={(e) => setConversion(parseInt(e.target.value))} style={{ fontSize: '1.4rem', fontWeight: '700' }} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="inputBeats" className="form-label">
+                    <label htmlFor="inputBeats" className="form-label" style={{ fontSize: '1.4rem', fontWeight: 'bold'}}>
                         Time Signature (Beats per Cycle)
                     </label>
-                    <input type="number" className="form-control" id="inputBeats" value={beatCycle} onChange={(e) => setBeatCycle(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" id="inputBeats" value={beatCycle} onChange={(e) => setBeatCycle(parseInt(e.target.value))} style={{ fontSize: '1.4rem', fontWeight: '700'}} />
                 </div>
             </div>
             </div>
