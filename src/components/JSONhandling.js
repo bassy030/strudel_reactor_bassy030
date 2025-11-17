f406e3c68004be30bfddba05ecfe1bb91a2be5b2
bassy030 <bassy030@mymail.unisa.edu.au>
2025-11-17 23:00:23 +1030

    Add keyboard shortcuts for JSON save and upload.
    Introduced 'ctrl+shift+s' to trigger JSON download and 'ctrl+shift+u' to trigger JSON upload using react-hotkeys-hook.

--- src/components/JSONhandling.js
+++ src/components/JSONhandling.js
@@ -1,4 +1,5 @@
-﻿import React from 'react';
+﻿import React, { useRef} from 'react';
+import { useHotkeys } from 'react-hotkeys-hook'
 // Dowload JSON file function
 // I have taken reference for download json part from the following site: https://medium.com/@gb.usmanumar/how-to-export-data-to-csv-json-in-react-js-ea45d940652a
 // I have taken reference for  file upload in react from the following sites: https://stackoverflow.com/questions/61707105/react-app-upload-and-read-json-file-into-variable-without-a-server
@@ -6,6 +7,7 @@ export default function JSONhandling({
     sliderVolume, musicPattern, bpm, conversion, beatCycle,
      setSliderVolume, setMusicPattern, setBpm, setConversion, setBeatCycle
 }) {
+    const fileRefShortCut = useRef(null);
     const downloadJSONFile = () => {
         const dataToDowload = {
             volume: sliderVolume,
@@ -49,6 +51,21 @@ export default function JSONhandling({
         fileJsonReader.readAsText(fileJson);
     }
 
+    useHotkeys('ctrl+shift+s', (event) => {
+        event.preventDefault();
+        downloadJSONFile();
+    });
+
+    const triggerUpload = () => {
+        if (fileRefShortCut.current) {
+            fileRefShortCut.current.click();
+        }
+    }
+    useHotkeys('ctrl+shift+u', (event) => {
+        event.preventDefault();
+        triggerUpload();
+    })
+
     return (
         <div className="card shadow mt-3">
             <div className="card-header">
@@ -57,7 +74,7 @@ export default function JSONhandling({
             <div className="card-body d-grid gap-2">
                 <button onClick={downloadJSONFile} className="btn btn-outline-success">Save JSON</button>
                 <label htmlFor="uploadJSONFile" className="btn btn-outline-info">Upload JSON</label>
-                <input type="file" id="uploadJSONFile" accept=".json" onChange={uploadJSONFile} style={{ display: "none" }} />
+                <input type="file" ref={fileRefShortCut} id="uploadJSONFile" accept=".json" onChange={uploadJSONFile} style={{ display: "none" }} />
             </div>
         </div>
     );
